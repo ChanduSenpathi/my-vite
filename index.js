cbc26d53eaea84d81cf0cd19da5059a3f63d074c
chandu senapathi <chandu.senapathi@neosoftmail.com>
2023-08-24 21:03:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import jsonServer from "json-server";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on :${PORT}`);
+});
